1fedd25f92c7b6a0929ec864daf8abb5d5f95166
miguelyarcemunoz-ai <miguel.yarcemunoz@inemjose.edu.co>
2026-09-05 16:17:21 +0000

    feat: reto 3 solucionado

--- src/main/java/com/colegiomagico/reto3/PocionCiclos.java
+++ src/main/java/com/colegiomagico/reto3/PocionCiclos.java
@@ -6,7 +6,11 @@ package com.colegiomagico.reto3;
 public class PocionCiclos {
 
     public static String preparar(int cantidad) {
-        return ""; // TODO: Cambia esto
+        String resultado = "";
+    for (int i = 0; i < cantidad; i++) {
+        resultado += "Pocion ";
+    }
+    return resultado;
     }
 
     public static void main(String[] args) {
