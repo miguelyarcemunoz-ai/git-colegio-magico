0f840901d463c89c5fdee09c573668675e59ee09
miguelyarcemunoz-ai <miguel.yarcemunoz@inemjose.edu.co>
2026-09-05 16:54:05 +0000

    feat: reto 9 solucionado

--- src/main/java/com/colegiomagico/reto9/InvocacionFamiliar.java
+++ src/main/java/com/colegiomagico/reto9/InvocacionFamiliar.java
@@ -5,9 +5,9 @@ package com.colegiomagico.reto9;
  */
 public class InvocacionFamiliar {
 
-    public static String invocar(String nombre, String tipo) {
-        return ""; // TODO: Cambia esto
-    }
+public static String invocar(String nombre, String tipo) {
+    return "Familiar: " + tipo + " llamado " + nombre;
+}
 
     public static void main(String[] args) {
         System.out.println(invocar("Hedwig", "Lechuza"));
